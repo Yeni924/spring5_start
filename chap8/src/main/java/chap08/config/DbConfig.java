943474c048f8aced3ac5a87bcfd8b1d6546f1389
kwonyeeun <ktech960924@gmail.com>
2021-06-19 12:54:35 +0900

    스프링 08장 DB연동

---
+++ chap8/src/main/java/chap08/config/DbConfig.java
@@ -0,0 +1,26 @@
+package chap08.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DbConfig {
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() {
+
+
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/test?serverTimezone=UTC");
+		ds.setUsername("root");
+		ds.setPassword("kwonyeeun96!");
+		ds.setInitialSize(2);
+		ds.setMaxActive(10);
+		ds.setTestWhileIdle(true);
+		ds.setMinEvictableIdleTimeMillis(60000 * 3);
+		ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
+		return ds;
+	}
+}
---
+++ chap8/src/main/java/chap08/spring/MemberNotFoundException.java
@@ -0,0 +1,5 @@
+package chap08.spring;
+
+public class MemberNotFoundException extends RuntimeException {
+
+}
